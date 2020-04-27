6fd6cb288c9119138147616215b3ad68f3997c07
donniegit777 <49219205+donniegit777@users.noreply.github.com>
2020-04-27 13:44:14 -0400

    Update __init__.py

--- greengrass_common/__init__.py
+++ greengrass_common/__init__.py
@@ -1,6 +1,2 @@
-#
-# Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-
 from .function_arn_fields import FunctionArnFields
 from .greengrass_message import GreengrassMessage
