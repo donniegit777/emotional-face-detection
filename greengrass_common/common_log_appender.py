5ff6a71198f08c8e533046e2887b90ccacc4cffe
donniegit777 <49219205+donniegit777@users.noreply.github.com>
2020-04-26 21:16:50 -0400

    Update common_log_appender.py

--- greengrass_common/common_log_appender.py
+++ greengrass_common/common_log_appender.py
@@ -1,10 +1,3 @@
-#
-# Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# this log appender is shared among all components of python lambda runtime, including:
-# greengrass_common/greengrass_message.py, greengrass_ipc_python_sdk/ipc_client.py,
-# greengrass_ipc_python_sdk/utils/exponential_backoff.py, lambda_runtime/lambda_runtime.py.
-# so that all log records will be emitted to local Cloudwatch.
 import logging.handlers
 
 from greengrass_common.local_cloudwatch_handler import LocalCloudwatchLogHandler
